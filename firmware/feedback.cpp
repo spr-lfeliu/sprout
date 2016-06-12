54ea5e04e317bf0072f7a7b317f55cfc5f880e05
Luis Feliu <spr-lfeliu@users.noreply.github.com>
2016-06-12 12:45:50 -0500

    Refactored feedback

--- firmware/feedback.cpp
+++ firmware/feedback.cpp
@@ -1,12 +1,12 @@
 #include "feedback.h"
 #include "application.h"
 
-Feedback::Feedback(int pin_ok, int pin_great, int pin_notgood)
+Feedback::Feedback(int pin_neutral, int pin_positive, int pin_negative)
 {
     Reset();
-    m_pin_ok = pin_ok;
-    m_pin_great = pin_great;
-    m_pin_notgood = pin_notgood;
+    m_pin_neutral = pin_neutral;
+    m_pin_positive = pin_positive;
+    m_pin_negative = pin_negative;
 }
 
 void Feedback::Reset() 
@@ -16,30 +16,30 @@ void Feedback::Reset()
 
 void Feedback::Setup()
 {
-    pinMode(m_pin_ok, OUTPUT);
-    pinMode(m_pin_great, OUTPUT);
-    pinMode(m_pin_notgood, OUTPUT);
+    pinMode(m_pin_neutral, OUTPUT);
+    pinMode(m_pin_positive, OUTPUT);
+    pinMode(m_pin_negative, OUTPUT);
 }
 
 void Feedback::TurnLedOn(Button button)
 {
     TurnAllLedsOff();
     switch(button) {
-        case ok:
-            digitalWrite(m_pin_ok, HIGH);
+        case neutral:
+            digitalWrite(m_pin_neutral, HIGH);
             break;
-        case great:
-            digitalWrite(m_pin_great, HIGH);
+        case positive:
+            digitalWrite(m_pin_positive, HIGH);
             break;
-        case notgood:
-            digitalWrite(m_pin_notgood, HIGH);
+        case negative:
+            digitalWrite(m_pin_negative, HIGH);
             break;
     }
 }
 
 void Feedback::TurnAllLedsOff()
 {
-    digitalWrite(m_pin_ok, LOW);
-    digitalWrite(m_pin_great, LOW);
-    digitalWrite(m_pin_notgood, LOW);
+    digitalWrite(m_pin_neutral, LOW);
+    digitalWrite(m_pin_positive, LOW);
+    digitalWrite(m_pin_negative, LOW);
 }
